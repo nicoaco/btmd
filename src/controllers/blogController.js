b7d4e0c66f94bfd927582df89ee271b2b21c921b
nicoaco <nicoaco@gmail.com>
2023-01-30 09:53:24 -0300

    Agregue ruta, controlador, data y modelo del blog -solo principal, falta individual-. Ediciones varias

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const blogModelo = require("../models/blog");
 
 const blogController = {
-    blog: (req,res)=> res.render(path.resolve(__dirname,'../views/blog.ejs'))
+    blog: (req,res)=> res.render(path.resolve(__dirname,'../views/blogs/blog-main.ejs'), {articulos : blogModelo.todos()}),
+   // detail: (req,res)=> res.render(path.resolve(__dirname,'../views/blogs/blog-main.ejs'))
 };
 
 module.exports = blogController;
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -3,8 +3,17 @@ const path = require('path');
 const router = express.Router();
 const blogController = require('../controllers/blogController');
 
-
+//rutas del blog
 router.get('/', blogController.blog);
 router.get('/:id', (req,res) => res.send('Bienvenido al Blog. Recurso ' + req.params.id));
+//router.get ("/:id", productController.detail);
+//router.get("/create", blogController.create);
+//router.get("/:id/edit", blogController.edit);
+
+//router.post("/create", [upload.single("img")] ,blogController.store);
+//router.put("/:id/edit", [upload.single("img")],blogController.update);
+//router.delete("/:id/delete",blogController.delete)
+
+
 
 module.exports = router;
